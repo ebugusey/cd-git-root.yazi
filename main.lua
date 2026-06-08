d12291512516ff73a4c745aedaafa493cbc7f261
ebugusey <ebugusey@users.noreply.github.com>
2026-06-08 13:23:07 +0400

    Make it work inside .git dir as well
    Use --show-cdup instead of --show-toplevel. Last one gives fatal error
    when run inside .git dir.

--- main.lua
+++ main.lua
@@ -4,32 +4,68 @@ local get_cwd = ya.sync(function()
 	return tostring(cx.active.current.cwd)
 end)
 
+--- @param args table<string, string>
+--- @param cwd string
+--- @return string[]|nil
+local function run_with(args, cwd)
+	local output = Command("git")
+		:arg(args)
+		:cwd(cwd)
+		:output()
+	if not output or not output.status.success then
+		return nil
+	end
+
+	ya.dbg(output.stdout)
+
+	local result = {}
+	local text = output.stdout:gsub("\r\n", "\n"):gsub("\r", "\n")
+	for line in text:gmatch("([^\n]*)\n") do
+		table.insert(result, line)
+	end
+
+	ya.dbg(result)
+
+	return result
+end
+
 local function get_git_toplevel()
-	local command =
-		Command("git")
-			:arg("rev-parse")
-			:arg("--show-toplevel")
-			:cwd(get_cwd())
-	local output = command:output()
+	local cwd = get_cwd()
+
+	local output = run_with({
+		"rev-parse",
+		"--is-bare-repository",
+		"--is-inside-git-dir",
+		"--git-dir",
+		"--show-cdup",
+	}, cwd)
 	if not output then
 		return nil
 	end
 
-	if output.status.code == 0 then
-		local destination = output.stdout:gsub("[\n\r]", "") .. "/"
-		return destination
-	else
+	local is_bare_repo = output[1] == "true"
+	local is_git_dir = output[2] == "true"
+	local git_dir = output[3]
+	local relative_up = output[4]
+
+	if is_bare_repo then
 		return nil
 	end
+
+	if is_git_dir then
+		local path = Url(git_dir)
+		return path.parent
+	end
+
+	return Url(relative_up)
 end
 
 return {
 	entry = function()
 		local destination = get_git_toplevel()
-		ya.dbg(destination)
+		ya.dbg(tostring(destination))
 		if destination then
-			local target = Url(destination)
-			ya.emit("cd", { target })
+			ya.emit("cd", { destination })
 		else
 			ya.notify({
 				title = "Could not change directory!",
